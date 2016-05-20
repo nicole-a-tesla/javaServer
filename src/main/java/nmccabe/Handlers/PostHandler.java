8de4a0c6ba5cf7ddc8085ef4bd12ed81ddbebd38
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-05-20 14:00:48 -0400

    Wire up PUT to write to files

--- src/main/java/nmccabe/Handlers/PostHandler.java
+++ src/main/java/nmccabe/Handlers/PostHandler.java
@@ -1,11 +1,19 @@
 package nmccabe.Handlers;
 
+import nmccabe.FileWriter;
 import nmccabe.Request;
 import nmccabe.Response;
 
+import java.io.IOException;
+
 public class PostHandler extends Handler {
     @Override
-    public Response getResponseFor(Request request) {
-        return buildResponseForStatus(OK_STATUS);
+    public Response getResponseFor(Request request) throws IOException {
+        Response response = buildResponseForStatus(OK_STATUS);
+
+        String postTo = System.getProperty("baseDir") + request.route;
+        new FileWriter().write(postTo, request.body);
+
+        return response;
     }
 }
--- src/test/java/PostHandlerTest.java
+++ src/test/java/PostHandlerTest.java
@@ -1,29 +1,56 @@
+import nmccabe.*;
+import nmccabe.FileReader;
 import nmccabe.Handlers.PostHandler;
 import nmccabe.Handlers.ResourceHandler;
-import nmccabe.Request;
-import nmccabe.Response;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.*;
+import java.util.HashMap;
+
+import static java.nio.file.Files.deleteIfExists;
 import static org.junit.Assert.assertEquals;
 
 public class PostHandlerTest {
+    String testFileName = "/testForm";
+    String testFilePath = System.getProperty("baseDir")  + testFileName;
+    Request postRequest;
+    File testFile;
+
+
+    @Before
+    public void setUp() throws IOException {
+        new Server().setUp(new HashMap());
+        postRequest = Helper.buildRequestFromString("POST " + testFileName + " HTTP/1.0\r\n\r\ndata=whoa\r\n\r\n");
+        testFile = new File(testFilePath);
+        testFile.createNewFile();
+    }
+
     @Test
     public void returns200OKForFormPost() throws Exception {
-        Request request = Helper.buildRequestFromString("POST /form HTTP/1.0\r\n\r\n");
-        Response response = new PostHandler().getResponseFor(request);
-
+        Response response = new PostHandler().getResponseFor(postRequest);
         assertEquals("200 OK", response.statusCode);
     }
 
     @Test
     public void writesToFile() throws Exception {
-        Request request = Helper.buildRequestFromString("POST /form HTTP/1.0\r\ndata=whoa\r\n\r\n");
-//        assertEquals("data=whoa", request.);
-        Response response = new PostHandler().getResponseFor(request);
-        assertEquals("200 OK", response.statusCode);
+        new PostHandler().getResponseFor(postRequest);
+        String actual = new FileReader().read(testFilePath);
+        assertEquals("data=whoa", actual);
+    }
 
-//        Request gRequest = Helper.buildRequestFromString("GET /form HTTP/1.0\r\n\r\n");
-//        Response gResponse = new ResourceHandler().getResponseFor(gRequest);
-//        assertEquals("data=whoa", new String(gResponse.body));
+    @Test
+    public void respondsWithBodyOfFileData() throws Exception {
+        new PostHandler().getResponseFor(postRequest);
+        Request getRequest = Helper.buildRequestFromString("GET " + testFileName + " HTTP/1.0\r\n\r\n");
+        Response getResponse = new ResourceHandler().getResponseFor(getRequest);
+
+        assertEquals("data=whoa\r\n\r\n", new String(getResponse.body));
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        deleteIfExists(testFile.toPath());
     }
 }
