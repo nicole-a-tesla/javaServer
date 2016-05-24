cd3b6e7277ff739b00c2c063f3d5857f0357f2d7
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-05-24 16:23:47 -0400

    Add append functionality to FileWriter

--- src/main/java/nmccabe/FileWriter.java
+++ src/main/java/nmccabe/FileWriter.java
@@ -4,15 +4,24 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileWriter {
-    public void write(String fileName, String writeThis) throws IOException {
-        byte data[] = writeThis.getBytes();
-
-        FileOutputStream out = new FileOutputStream(fileName);
-        out.write(data);
-        out.close();
-    }
 
     public void deleteFrom(String deleteFrom) throws IOException {
         write(deleteFrom, "");
+    }
+
+    public void write(String fileName, String writeThis) throws IOException {
+        FileOutputStream out = new FileOutputStream(fileName);
+        writeOrAppend(out, writeThis);
+    }
+
+    public void append(String fileName, String writeThis) throws IOException {
+        FileOutputStream out = new FileOutputStream(fileName, true);
+        writeOrAppend(out, writeThis);
+    }
+
+    private void writeOrAppend(FileOutputStream out, String writeThis) throws IOException {
+        byte[] data = writeThis.getBytes();
+        out.write(data);
+        out.close();
     }
 }
