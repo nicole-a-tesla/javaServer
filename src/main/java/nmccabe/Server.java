3b7be77818550d9940fdd053c25deb3242c70635
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-05-26 14:54:16 -0400

    Refactor

--- src/main/java/nmccabe/Server.java
+++ src/main/java/nmccabe/Server.java
@@ -24,9 +24,10 @@ public class Server {
 
         while (!serverSocket.isClosed()) {
             Socket clientSocket = serverSocket.accept();
-            executioner.execute(new ServerWorker(clientSocket, logger));
+            OutputStream clientOut = clientSocket.getOutputStream();
+            InputStream clientIn = clientSocket.getInputStream();
+            executioner.execute(new ServerWorker(clientIn, clientOut, logger));
         }
-        executioner.shutdown();
     }
 
 }
--- src/main/java/nmccabe/ServerWorker.java
+++ src/main/java/nmccabe/ServerWorker.java
@@ -5,38 +5,36 @@ import nmccabe.Handlers.Handler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.Socket;
 
 public class ServerWorker implements Runnable {
-    Socket clientSocket;
+    InputStream clientIn;
+    OutputStream clientOut;
     Logger logger;
 
-    public ServerWorker(Socket clientSocket, Logger logger) {
-        this.clientSocket = clientSocket;
+    public ServerWorker(InputStream clientIn, OutputStream clientOut, Logger logger) {
+        this.clientIn = clientIn;
+        this.clientOut = clientOut;
         this.logger = logger;
     }
 
     @Override
     public void run() {
         try {
-            InputStream rawInputStream = clientSocket.getInputStream();
-            Request request = new RequestBuilder().build(rawInputStream);
+            Request request = new RequestBuilder().build(clientIn);
             logger.log(request);
             Handler handler = new Router().getHandlerFor(request);
             Response response = handler.getResponseFor(request);
 
+            new ResponsePrinter(response, clientOut).printToOutStream();
+            tearDownStream(clientOut);
 
-            OutputStream outStream;
-            if (!clientSocket.isClosed()) {
-                outStream = clientSocket.getOutputStream();
-                ResponsePrinter printer = new ResponsePrinter(response, outStream);
-                printer.printToOutStream();
-                outStream.flush();
-                outStream.close();
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private void tearDownStream(OutputStream out) throws IOException {
+        out.flush();
+        out.close();
     }
 }
